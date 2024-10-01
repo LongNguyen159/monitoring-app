d66127765b3f7eb01c9251e343b0985ce840064b
long <nguyenthanhlong@morenergy.net>
2024-10-01 11:22:28 +0200

    WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MonitorService } from './services/monitor.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, MatSlideToggleModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'monitoring-app';
 
+  monitoringService = inject(MonitorService)
+  metrics: any;
+
   constructor() {
-    console.log('hello world!')
+    document.body.classList.add('dark-theme');
+  }
+
+  ngOnInit() {
+    this.monitoringService.monitorWebsite('/api').subscribe({
+      next: (response) => {
+        console.log('Response received:', response);
+        this.metrics = response
+      },
+      error: (err: HttpErrorResponse) => {
+        console.error('Error occurred:', err);
+      }
+    })
+  }
+
+  ngOnDestroy() {
+    console.log('AppComponent destroyed');
   }
 }
---
+++ src/app/services/monitor.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { interval, map, startWith, switchMap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MonitorService {
+
+  constructor(private http: HttpClient) { }
+
+  monitorWebsite(url: string) {
+    return interval(5000).pipe(
+      startWith(0),
+      switchMap(() => {
+        const startTime = performance.now(); // Start timer for each request
+        return this.http.get(url, { observe: 'response', responseType: 'text' }).pipe(
+          map(response => {
+            const endTime = performance.now(); // End timer
+            const responseTime = endTime - startTime; // Calculate response time
+            const statusCode = response.status; // Get HTTP status code
+            const contentSize = response.body?.length; // Size of the response body
+  
+            return {
+              responseTime,
+              statusCode,
+              contentSize,
+              // Add more metrics as needed
+            };
+          })
+        );
+      })
+    );
+  }
+}
